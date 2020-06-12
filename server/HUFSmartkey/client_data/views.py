c6d4d29d0deaae10bc01827e510cb92e2476db1c
ujin2021 <ujin2021@naver.com>
2020-06-12 21:05:00 +0900

    yj_0612_2104

--- server/HUFSmartkey/client_data/views.py
+++ server/HUFSmartkey/client_data/views.py
@@ -6,10 +6,6 @@ from .serializers import ClientDataSerializer
 from rest_framework.parsers import JSONParser
 from django.contrib.auth import authenticate
 
-# from rest_framework.decorators import api_view
-
-
-#@api_view(['GET', 'POST'])
 @csrf_exempt
 def client_list(request, format=None):
     if request.method == 'GET': # 전체조회
@@ -17,15 +13,26 @@ def client_list(request, format=None):
         serializer = ClientDataSerializer(query_set, many=True)
         return JsonResponse(serializer.data, safe=False)
     
-    elif request.method == 'POST': # 회원가입
-        data = JSONParser().parse(request)
-        serializer = ClientDataSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse({'code':'201', 'msg':'signup success'}, status=201)
-        return JsonResponse({'code':'400', 'msg':'signup fail'}, status=400)
+    elif request.method == 'POST': # 회원가입_test완료
+        identification = request.POST.get('identification', '')
+        password = request.POST.get('password', '')
+        phone_number = request.POST.get('phone_number', '')
+        name = request.POST.get('name', '')
 
-def login(request):
+        print('identification = ' + identification + 'password = ' + password + 'phone_number = ' + phone_number + 'name= ' + name) # 서버쪽 터미널에 띄움
+        myuser = ClientData.objects.filter(identification=identification)
+
+        if myuser: # db에 저장되어있으면 -> id중복
+            print("duplicated id, signUp failed") # for server debugging
+            return JsonResponse({'code':'400', 'msg':'duplicated id'}, status=400)
+        else: # new client면 -> db저장
+            form = ClientData(identification=identification, password=password, phone_number=phone_number, name=name)
+            form.save()
+            print("signUp success") # for server debugging
+            return JsonResponse({'code':'201', 'msg':'signup success'}, status=201) # app으로 보내는 msg
+        
+
+def login(request): # test하기전..
     if request.method == 'POST':
         data = JSONParser().parse(request)
         client_id = data['id']
@@ -35,4 +42,4 @@ def login(request):
             return HttpResponse(status=200)
         else:
             return HttpResponse(status=400)
-        # password 넘길때 암호화 필요.
+        # password 넘길때 암호화 필요. -> 추가하기

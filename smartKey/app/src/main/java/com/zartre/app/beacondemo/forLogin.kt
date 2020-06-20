5aeb346cdf13cb012a0121a7472104ab1a913dbd
JaesungLeee <viki0114@naver.com>
2020-06-21 03:00:50 +0900

    jaesung_0621_0300

--- smartKey/app/src/main/java/com/zartre/app/beacondemo/forLogin.kt
+++
@@ -1,7 +0,0 @@
-package com.zartre.app.beacondemo
-
-data class forLogin(
-    val code: String,
-    val msg: String,
-    val allowed_area: String
-)
--- smartKey/app/src/main/java/com/zartre/app/beacondemo/forSignup.kt
+++
@@ -1,7 +0,0 @@
-package com.zartre.app.beacondemo
-
-// String type code and message
-data class forSignup(
-    val code: String,
-    val msg: String
-)
